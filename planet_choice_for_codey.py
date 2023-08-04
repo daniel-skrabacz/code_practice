d964c82bc579bfd756ee913755e9123789138fbc
Daniel <daniel-skrabacz@users.noreply.github.com>
2023-08-04 16:27:25 -0500

    Update planet_choice_for_codey.py
    change mass to weight

--- planet_choice_for_codey.py
+++ planet_choice_for_codey.py
@@ -4,25 +4,25 @@ print("   1. Venus   2. Mars    3. Jupiter")
 print("   4. Saturn  5. Uranus  6. Neptune\n")
  
 weight = 185
-planet = 3
+planet = 2
 mass = weight/2.34
 # Write an if statement below:
 
 if planet == 1:
   print('Venus!')
-  print(mass*0.91)
+  print(weight*0.91)
 elif planet == 2:
   print('Mars')
-  print(mass*0.38)
+  print(weight*0.38)
 elif planet == 3:
   print('Jupiter')
-  print(mass*2.34)
+  print(weight*2.34)
 elif planet == 4:
   print('Saturn')
-  print(mass*1.06)
+  print(weight*1.06)
 elif planet == 5:
   print('Uranus')
-  print(mass*0.92)
+  print(weight*0.92)
 elif planet == 6:
   print('Neptune')
-  print(mass*1.19)
+  print(weight*1.19)
